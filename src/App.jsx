dbf0b5129d2e490e5f270816a73923b03c75ad9e
kbario <kylebario1@gmail.com>
2022-05-03 11:38:45 +0800

    added boilerplate to components

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Header from './components/Header';
+import Project from './components/Project';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <main>
+      <Header/>
+      <Project/>
+      <Footer/>
+    </main>
+  );
+}
+
+export default App;
